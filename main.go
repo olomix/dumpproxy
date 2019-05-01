fc001b3b3ee63ec612fe6e427f1047d28f0fcd4e
Oleg Lomaka <oleg.lomaka@gmail.com>
2019-05-01 15:22:23 +0300

    get uniq file name prefix to save request/response data

--- main.go
+++ main.go
@@ -3,10 +3,14 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
 	"io"
 	"log"
 	"net"
 	"net/http"
+	"os"
+	"path"
+	"strconv"
 	"time"
 )
 
@@ -14,6 +18,7 @@ var listenAddr = flag.String("listen-addr", "localhost:8080", "listen address")
 var upstreamAddr = flag.String(
 	"upstream-addr", "localhost:80", "upstream address",
 )
+var dumpDir = flag.String("dir", "./", "directory to dump traffic")
 
 var dialer = &net.Dialer{
 	Timeout:   30 * time.Second,
@@ -74,8 +79,32 @@ func proxy(w http.ResponseWriter, r *http.Request) {
 		log.Print(err)
 		return
 	}
+	fNamePrefix, err := fname()
+	if err != nil {
+		log.Print(err)
+	}
+	_ = fNamePrefix
 
-	log.Printf("%v %v %v %v", r.Host, resp.StatusCode, bodySize, r.URL)
+	log.Printf("%v %v %v %v %v", r.Host, resp.StatusCode, bodySize, r.URL, fNamePrefix)
+}
+
+func fname() (string, error) {
+	datePrefix := path.Join(*dumpDir, time.Now().Format("2006-01-02-15-04-05-"))
+	idx := 0
+	var prefix string
+	for {
+		prefix = datePrefix + strconv.Itoa(idx)
+		fname := prefix + ".request_headers"
+		f, err := os.OpenFile(fname, os.O_RDWR|os.O_CREATE|os.O_EXCL, 0666)
+		if err != nil {
+			if os.IsExist(err) {
+				idx++
+				continue
+			}
+			panic(err)
+		}
+		return prefix, f.Close()
+	}
 }
 
 func closeLogError(closer io.Closer) {
@@ -91,5 +120,14 @@ func closeLogError(closer io.Closer) {
 
 func main() {
 	flag.Parse()
+
+	fileInfo, err := os.Stat(*dumpDir)
+	if err != nil {
+		panic(err)
+	}
+	if !fileInfo.IsDir() {
+		panic(fmt.Sprintf("%v is not a directory", *dumpDir))
+	}
+
 	panic(http.ListenAndServe(*listenAddr, http.HandlerFunc(proxy)))
 }
